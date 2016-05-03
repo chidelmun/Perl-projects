db4624188b87b67fbc0207397b44de026f5654d2
Becca Tuck <beccatuck0@gmail.com>
2016-05-03 23:33:38 +0100

    Finished up data-types

--- data-types/data-types.pl
+++ data-types/data-types.pl
@@ -14,4 +14,16 @@ friends are :\n $friends[0],\n $friends[1], \n $friends[2],\n $friends[3] \n" );
 %hash = ('Dario',25, 'Nyah',28);
 print ("$hash{'Dario'} \n");
 
+print (__FILE__ .__LINE__. "\n");
+
+print (scalar @friends . "\n");
+
+print ("@friends\n");
+push(@friends, "Ahidjo");
+print("@friends\n");
+
+if (20 gt 15) {
+	print "True\n";
+}
+
 # End of program
