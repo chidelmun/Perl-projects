33cfa20e7ef05e20232116b2d400c3fe4ad76c85
Becca Tuck <beccatuck0@gmail.com>
2016-05-03 23:13:09 +0100

    Added support for perl hash

--- data-types/data-types.pl
+++ data-types/data-types.pl
@@ -8,6 +8,10 @@ $height = 1.79;
 
 
 print (" My name is : $Name\n I am $age years old and I am \n %height m tall \n My 
-friends are : $friends[0],\n $friends[1], \n $friends[2],\n $friends[3] \n" );
+friends are :\n $friends[0],\n $friends[1], \n $friends[2],\n $friends[3] \n" );
+
+
+%hash = ('Dario',25, 'Nyah',28);
+print ("$hash{'Dario'} \n");
 
 # End of program
