ec7d1046b6f6d6849ea4524fe4c07f1b937f45a5
Becca Tuck <beccatuck0@gmail.com>
2016-05-03 23:39:45 +0100

    finished with loops

---
+++ loops/loops.pl
@@ -0,0 +1,17 @@
+#Perl conditional statements and Loops
+
+if (55 gt 77) {
+	print("This is true\n")
+}else{
+	print("This is not True\n");
+}
+
+print( localtime()."\n");
+
+while (5 > 30) {
+	print("This will always be true\n");
+}
+
+for (my $var = 0; $var < 10; $var++) {
+	print($var."\n"); 
+}
