c64776f92717b2a516bfab7de58f1097933ca5fc
Becca Tuck <beccatuck0@gmail.com>
2016-05-03 22:57:34 +0100

    Done with variables

--- variables/variables.pl
+++ variables/variables.pl
@@ -1,12 +1,19 @@
 # This tutorial explains how to use perl Variables
 
 
-$var = 25 #Declares a variable
+$var = 25; #Declares a variable
 
 print("The Value of var = $var\n"); # prints value of Var
 
-$var++
+$var++;
 
 print("Value of var is now = $var \n");
 
 #Enf of program
+
+$str = <<"FIN";
+	This is a very long string variable that can span
+	multiple lines and is stored in the $str variable
+FIN
+
+print "$str";
