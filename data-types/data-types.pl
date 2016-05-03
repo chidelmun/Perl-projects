481709d6eb1ff90270a3d10b7c3e93a622e96ba0
Becca Tuck <beccatuck0@gmail.com>
2016-05-03 23:09:34 +0100

    Added scalar and Array examples

---
+++ data-types/data-types.pl
@@ -0,0 +1,13 @@
+# Perl Datatypes
+
+$age = 25;
+$Name = "Dario";
+$height = 1.79;
+
+@friends = ("Nyah", "Acho", "Izak", "Teche", "GNU");
+
+
+print (" My name is : $Name\n I am $age years old and I am \n %height m tall \n My 
+friends are : $friends[0],\n $friends[1], \n $friends[2],\n $friends[3] \n" );
+
+# End of program
