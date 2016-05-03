d474972c23c74b2350c24a10be81ae4ae4fadfeb
Becca Tuck <beccatuck0@gmail.com>
2016-05-03 22:49:27 +0100

    introducing perl variables

---
+++ variables/variables.pl
@@ -0,0 +1,12 @@
+# This tutorial explains how to use perl Variables
+
+
+$var = 25 #Declares a variable
+
+print("The Value of var = $var\n"); # prints value of Var
+
+$var++
+
+print("Value of var is now = $var \n");
+
+#Enf of program
