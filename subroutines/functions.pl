2dc89f36828dd274714e5695ebe500d192668fdf
Becca Tuck <beccatuck0@gmail.com>
2016-05-04 22:43:37 +0100

    Apssing arguments to functions

--- subroutines/functions.pl
+++ subroutines/functions.pl
@@ -11,3 +11,21 @@ sub get_double {
 }
 
 print(get_double() . "\n");
+
+
+sub max{
+	if ($_[0] > $_[1]) {
+		return $_[0];
+	}else{
+		return $_[1];
+	}
+}
+
+
+sub getmax{
+	return $_[0]>$_[1]?$_[0]:$_[1];
+}
+
+print(max(5,10)."\n");
+
+print(getmax(25,8)."\n");
